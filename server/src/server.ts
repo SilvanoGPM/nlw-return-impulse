d49415c75d63cb9d908a23111b6f21aa62a9b0af
SkyG0D <silvanosilvino@hotmail.com>
2022-05-07 20:51:30 -0300

    feat: feedback route created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+import express from "express";
+
+import { prisma } from "./prisma";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send('ok');
+app.use(express.json());
+
+app.post("/feedbacks", async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
-  console.log(`server running on http://localhost:3333`)
+  console.log(`server running on http://localhost:3333`);
 });
