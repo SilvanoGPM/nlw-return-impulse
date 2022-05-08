0817e4c53d36a44380e1bfc44fa931426d92fd8b
SkyG0D <silvanosilvino@hotmail.com>
2022-05-08 01:26:54 -0300

    feat: add cors config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { routes } from './routes';
 
@@ -6,9 +7,9 @@ const PORT = process.env.PORT || 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
